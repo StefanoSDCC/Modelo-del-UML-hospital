939116d58564c8725c2684a8b68bbf7b2909b84a
StefanoSDCC <166523262+StefanoSDCC@users.noreply.github.com>
2024-05-21 09:53:47 -0500

    Update doctor.java

--- doctor.java
+++ doctor.java
@@ -1,17 +1,37 @@
-public class Doctor extends Persona {
-    private String especialidad;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Doctor(String nombre, String identificacion, String direccion, String especialidad) {
-        super(nombre, identificacion, direccion);
+public class Doctor extends Persona implements GestionCitas {
+    private final String especialidad;
+    private List<CitaMedica> citas;
+
+    public Doctor(String nombre, String id, String direccion, String especialidad) {
+        super(nombre, id, direccion);
         this.especialidad = especialidad;
+        this.citas = new ArrayList<>();
     }
 
-    
     public String getEspecialidad() {
         return especialidad;
     }
 
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
+    public List<CitaMedica> getCitas() {
+        return citas;
+    }
+
+    @Override
+    public void programarCita(CitaMedica cita) {
+        cita.setEstado("programada");
+        citas.add(cita);
+    }
+
+    @Override
+    public void cancelarCita(CitaMedica cita) {
+        cita.setEstado("cancelada");
+    }
+
+    @Override
+    public void realizarCita(CitaMedica cita) {
+        cita.setEstado("realizada");
     }
 }
