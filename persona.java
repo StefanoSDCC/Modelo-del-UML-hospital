35295803fb3e841186f3b01a16adfba19ef50b0e
StefanoSDCC <166523262+StefanoSDCC@users.noreply.github.com>
2024-05-21 09:15:54 -0500

    Create persona.java

---
+++ persona.java
@@ -0,0 +1,36 @@
+public class Persona {
+    private String nombre;
+    private String identificacion;
+    private String direccion;
+
+    public Persona(String nombre, String identificacion, String direccion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.direccion = direccion;
+    }
+
+  
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
