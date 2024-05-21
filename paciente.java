539301222658497bbda4bee8c534d7ee9a69cccc
StefanoSDCC <166523262+StefanoSDCC@users.noreply.github.com>
2024-05-21 09:50:41 -0500

    Update paciente.java

--- paciente.java
+++ paciente.java
@@ -1,21 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Paciente extends Persona {
-    private List<Cita> citas;
+    private ExpedienteMedico expediente;
+    private List<CitaMedica> citas;
 
-    public Paciente(String nombre, String identificacion, String direccion) {
-        super(nombre, identificacion, direccion);
-        citas = new ArrayList<>();
+    public Paciente(String nombre, String id, String direccion, ExpedienteMedico expediente) {
+        super(nombre, id, direccion);
+        this.expediente = expediente;
+        this.citas = new ArrayList<>();
     }
 
+    public ExpedienteMedico getExpediente() {
+        return expediente;
+    }
 
-    public List<Cita> getCitas() {
+    public void setExpediente(ExpedienteMedico expediente) {
+        this.expediente = expediente;
+    }
+
+    public List<CitaMedica> getCitas() {
         return citas;
     }
 
-    public void agregarCita(Cita cita) {
-        citas.add(cita);
+    public void addCita(CitaMedica cita) {
+        this.citas.add(cita);
     }
 }
+
